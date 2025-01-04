cd446868ebd44901f9575180e74f9fb288bc6938
Sanket Deb <imsanketdeb20@gmail.com>
2025-01-04 22:48:56 +0530

    TMDB API connection setup

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,17 @@
 export const LOGO =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
 export const POSTER =
   "https://assets.nflxext.com/ffe/siteui/vlv3/74d734ca-0eab-4cd9-871f-bca01823d872/web/IN-en-20241021-TRIFECTA-perspective_2277eb50-9da3-4fdf-adbe-74db0e9ee2cf_small.jpg";
+
 export const USER_ICON =
   "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDQ5M2FkNjBiYTI3MDYxY2E1MGFmNWNkM2IxODUxOSIsIm5iZiI6MTczNTkyODY0Ni45Njk5OTk4LCJzdWIiOiI2Nzc4MmI0NmMxMWVlNjFkZDg3NGFlNWIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Nxb0yu4OulYcG4bxFG-JG2eIfBuwgVkX_4X-WoNf1iU",
+  },
+};
