906d0875351b3c475cbf0e6f86af02ef36b846fe
Sanket Deb <imsanketdeb20@gmail.com>
2025-01-08 13:15:59 +0530

    claening code and implementing multiple language for gpt search page (trial)

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
-import { LOGO, USER_ICON } from "../utils/constants";
+import { LOGO, SUPPORTED_LANGUAGES, USER_ICON } from "../utils/constants";
 import { toggleGptSearchView } from "../utils/gptSlice";
 
 const Header = () => {
@@ -43,11 +43,25 @@ const Header = () => {
     dispatch(toggleGptSearchView());
   };
 
+  const handleLanguageChange = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-44" src={LOGO} alt="Logo"></img>
       {user && (
         <div className="flex p-2">
+          <select
+            className="p-2 m-2 bg-gray-900 text-white rounded-lg"
+            onChange={handleLanguageChange}
+          >
+            {SUPPORTED_LANGUAGES.map((lang) => (
+              <option key={lang.identifier} value={lang.identifier}>
+                {lang.name}
+              </option>
+            ))}
+          </select>
           <button
             className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
             onClick={handleGptSearchClick}
--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -16,11 +16,9 @@ const useMovieTrailer = (movieId) => {
       API_OPTIONS
     );
     const json = await data.json();
-    console.log(json);
 
     const filterData = json.results.filter((video) => video.type === "Trailer");
     const trailer = filterData.length ? filterData[0] : json.result[0];
-    console.log(trailer);
     dispatch(addTrailerVideo(trailer));
   };
   useEffect(() => {
