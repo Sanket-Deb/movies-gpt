2896d47a0b0461789f0c7f796bef2b4e89a171d8
Sanket Deb <imsanketdeb20@gmail.com>
2025-01-09 19:28:01 +0530

    multilingual gpt page implementation

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
 import { LOGO, SUPPORTED_LANGUAGES, USER_ICON } from "../utils/constants";
 import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -44,7 +46,7 @@ const Header = () => {
   };
 
   const handleLanguageChange = (e) => {
-    console.log(e.target.value);
+    dispatch(changeLanguage(e.target.value));
   };
 
   return (
@@ -52,21 +54,23 @@ const Header = () => {
       <img className="w-44" src={LOGO} alt="Logo"></img>
       {user && (
         <div className="flex p-2">
-          <select
-            className="p-2 m-2 bg-gray-900 text-white rounded-lg"
-            onChange={handleLanguageChange}
-          >
-            {SUPPORTED_LANGUAGES.map((lang) => (
-              <option key={lang.identifier} value={lang.identifier}>
-                {lang.name}
-              </option>
-            ))}
-          </select>
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white rounded-lg"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
           <button
             className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
             onClick={handleGptSearchClick}
           >
-            GPT Search
+            {showGptSearch ? "Homepage" : "GPT Search"}
           </button>
           <img className="w-12 h-12 mr-2" alt="usericon" src={USER_ICON} />
           <button onClick={handleSignOut} className="font-bold text-white">
