ef6df70ab480468c044212854081c6de8e4fc6a3
Sanket Deb <imsanketdeb20@gmail.com>
2025-01-15 00:07:09 +0530

    Using the OpenAI API to get search query results

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
 import lang from "../utils/languageConstants";
 import { useSelector } from "react-redux";
+import openai from "../utils/openai";
 
 const GptSearchBar = () => {
   const langKey = useSelector((store) => store.config.lang);
 
+  const searchText = useRef(null);
+
+  const handleGptSearchClick = async () => {
+    console.log(searchText.current.value);
+    //Make an API call to GPT API and get Movie Results
+
+    const gptQuery =
+      "Act as a Movie Recommendation system and suggest some movies for the query" +
+      searchText.current.value +
+      ", only give me the top 5 movies, in form of list with one line descriptions like the examples results given ahead. Example Result: (1) (Movie Name 1): This movie is about abc (2) (1) (Movie Name 2): This movie is about xyz ... till top 5";
+
+    const gptResults = await openai.chat.completions.create({
+      messages: [{ role: "user", content: gptQuery }],
+      model: "gpt-3.5-turbo",
+    });
+    console.log(gptResults.choices);
+  };
+
   return (
     <div className="pt-[10%] flex justify-center">
-      <form className="w-1/2 bg-black grid grid-cols-12">
+      <form
+        className="w-1/2 bg-black grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
+          ref={searchText}
           type="text"
           className="p-4 m-4 col-span-9"
           placeholder={lang[langKey].gptSearchPlaceholder}
         />
-        <button className="m-4 col-span-3 py-2 px-4 bg-red-700 text-white rounded-lg">
+        <button
+          className="m-4 col-span-3 py-2 px-4 bg-red-700 text-white rounded-lg"
+          onClick={handleGptSearchClick}
+        >
           {lang[langKey].search}
         </button>
       </form>
