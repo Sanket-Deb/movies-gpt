249d7d560b73135c2d3295ff5251aa067380375c
Sanket Deb <imsanketdeb20@gmail.com>
2025-01-15 13:52:29 +0530

    Using google gemini api for search cause it's free

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import lang from "../utils/languageConstants";
 import { useSelector } from "react-redux";
 import openai from "../utils/openai";
+import { GEMINI_KEY } from "../utils/constants";
 
 const GptSearchBar = () => {
   const langKey = useSelector((store) => store.config.lang);
@@ -11,17 +12,18 @@ const GptSearchBar = () => {
   const handleGptSearchClick = async () => {
     console.log(searchText.current.value);
     //Make an API call to GPT API and get Movie Results
+    const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+    const genAI = new GoogleGenerativeAI(GEMINI_KEY);
 
     const gptQuery =
       "Act as a Movie Recommendation system and suggest some movies for the query" +
       searchText.current.value +
-      ", only give me the top 5 movies, in form of list with one line descriptions like the examples results given ahead. Example Result: (1) (Movie Name 1): This movie is about abc (2) (1) (Movie Name 2): This movie is about xyz ... till top 5";
+      ". only give me names of 5 movies, comma separated like the given result ahead. Example Result: Avatar, Sholay, Bahubali, Singham, Once upon a time in mumbai";
 
-    const gptResults = await openai.chat.completions.create({
-      messages: [{ role: "user", content: gptQuery }],
-      model: "gpt-3.5-turbo",
-    });
-    console.log(gptResults.choices);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const gptResults = await model.generateContent(gptQuery);
+    console.log(gptResults.response.text());
   };
 
   return (
