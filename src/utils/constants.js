e1971980ea898f84a097eddd0f4311ebfcfaf33a
Sanket Deb <imsanketdeb20@gmail.com>
2025-01-24 22:52:03 +0530

    Minor update - key fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,6 +23,6 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "spanish", name: "Spanish" },
 ];
 
-export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY; //NOT USING
 
 export const GEMINI_KEY = process.env.REACT_APP_GEMINI_KEY;
