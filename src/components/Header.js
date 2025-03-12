4d4e277f5722f18344987a180e522a402ed7175f
Sanket Deb <imsanketdeb20@gmail.com>
2025-03-12 22:14:57 +0530

    making the header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -50,8 +50,8 @@ const Header = () => {
   };
 
   return (
-    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img className="w-44" src={LOGO} alt="Logo"></img>
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between ">
+      <img className="w-44 mx-auto md:m" src={LOGO} alt="Logo"></img>
       {user && (
         <div className="flex p-2">
           {showGptSearch && (
